b64bd71eab53a4bb7074a1a6076868f498f861ac
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-05 18:18:03 +0200

    dealing with btn styles

--- src/layout/components/header/header.component.tsx
+++ src/layout/components/header/header.component.tsx
@@ -2,13 +2,41 @@ import React from "react";
 import classComponents from "./header.styles";
 
 export const HeaderComponent: React.FC = () => {
-  const { Container, InputField, Input, Label } = classComponents;
+  const {
+    Container,
+    FlexContainer,
+    InputField,
+    Input,
+    Label,
+    Button,
+  } = classComponents;
   return (
     <Container>
-      <InputField>
-        <Label>Número</Label>
-        <Input type="text" />
-      </InputField>
+      <FlexContainer>
+        <InputField>
+          <Label>Número</Label>
+          <Input type="text" id="number" />
+        </InputField>
+        <InputField>
+          <Label>Proveedor</Label>
+          <Input type="text" />
+        </InputField>
+        <InputField>
+          <Label>Fecha</Label>
+          <Input type="date" />
+        </InputField>
+      </FlexContainer>
+      <FlexContainer>
+        <InputField>
+          <Label>Importe total</Label>
+          <Input type="number" />
+        </InputField>
+        <InputField>
+          <Label>Estado</Label>
+          <Input type="text" />
+        </InputField>
+        <Button>Enviar</Button>
+      </FlexContainer>
     </Container>
   );
 };
